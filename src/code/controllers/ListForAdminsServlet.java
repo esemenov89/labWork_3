fa5355c77e12ffd5f2f2415cb99509045b0bdf96
esemenov89 <00semen00@gmail.com>
2017-04-26 14:02:29 +0300

    ololo

--- src/code/controllers/ListForAdminsServlet.java
+++ src/code/controllers/ListForAdminsServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
+/** ololo
  * Created by admin on 22.04.2017.
  */
 public class ListForAdminsServlet  extends HttpServlet {
